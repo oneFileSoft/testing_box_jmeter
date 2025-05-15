532db1839bb9e3458e3aa33486dda6e37c2b7b45
Slava <splakhin@yahoo.com>
2025-05-14 22:35:43 -0500

    new chages

--- utils/flattenWithPuppeteer.js
+++ utils/flattenWithPuppeteer.js
@@ -1,13 +1,19 @@
+// utils/flattenWithPuppeteer.js
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const inputPath = process.argv[2];
-  const outputPath = process.argv[3];
+  const [,, inputPath, outputPath] = process.argv;
 
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ]
+  });
+
   const page = await browser.newPage();
-
   await page.goto(`file://${process.cwd()}/${inputPath}`, { waitUntil: 'networkidle0' });
 
   const html = await page.content();
